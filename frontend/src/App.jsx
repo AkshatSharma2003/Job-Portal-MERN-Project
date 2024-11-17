6796b5d4686c61d779fea7bb54fc720bfcdb37aa
Akshat Sharma <akshatsharmavds@gmail.com>
2024-11-17 21:48:59 +0530

    updated commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home  from './components/Home'
+
+
+function App() {
+  
+const appRouter = createBrowserRouter([
+  {
+  path:'/',
+  element:<Home/>
+  },
+  {
+  path:'/login',
+  element:<Login/>
+  },
+  {
+  path:'/signup',
+  element:<Signup/>
+  },
+])
+
+  return (
+    <>
+      <RouterProvider router = {appRouter}/>
+    </>
+  )
+}
+
+export default App
